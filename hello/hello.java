01249caf552ae1c31efdf95e20aaf6b504e014d4
vidya <vid100gowda@gmail.com>
2018-11-27 09:08:15 -0600

    added awsdabs :

--- hello/hello.java
+++ hello/hello.java
@@ -1,4 +1,4 @@
-public class Main {
+class vidya{
     public static void main(String[] args){                                                                                                                                            
         System.out.println("Main Class Start");                                                                                                                                        
     }  
